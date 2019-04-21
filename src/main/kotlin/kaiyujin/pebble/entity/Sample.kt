e185f42c1e260ae4c2c39b9d7ef7732416167f9e
daisuke shiraishi <kaiyujin@gmail.com>
2019-04-21 20:02:10 +0900

    Add valid

--- src/main/kotlin/kaiyujin/pebble/entity/Sample.kt
+++ src/main/kotlin/kaiyujin/pebble/entity/Sample.kt
@@ -1,8 +1,12 @@
 package kaiyujin.pebble.entity
 
 import org.seasar.doma.Entity
+import javax.validation.constraints.NotEmpty
+import javax.validation.constraints.Size
 
 @Entity(immutable = true)
 data class Sample(
+    @field:NotEmpty
+    @field:Size(min = 2, max = 10)
     val name: String
 )
